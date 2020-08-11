7d34b7bf37f2b9b857c50769e4c7763440eafbbb
Tobias Bales <tobias.bales@gmail.com>
2020-08-11 19:31:49 +0200

    Display message if all linters are already installed

--- lib/rails_starter/cli.rb
+++ lib/rails_starter/cli.rb
@@ -15,14 +15,22 @@ module RailsStarter
     end
 
     sig { void }
+    # rubocop:disable Metrics/MethodLength
     def start
-      linters = @prompt.multi_select('Which linters do you want?') do |menu|
-        RailsStarter::Linters.for_path(Dir.getwd).each do |linter, component|
-          menu.choice linter, component, disabled: component.met? ? '(already installed)' : nil
-        end
-      end
+      linter_options = RailsStarter::Linters.for_path(Dir.getwd)
 
-      linters.each(&:meet)
+      if linter_options.values.any? { |l| !l.met? }
+        linters = @prompt.multi_select('Which linters do you want?') do |menu|
+          RailsStarter::Linters.for_path(Dir.getwd).each do |linter, component|
+            menu.choice linter, component, disabled: component.met? ? '(already installed)' : nil
+          end
+        end
+
+        linters.each(&:meet)
+      else
+        puts 'All available linters are already installed'
+      end
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
